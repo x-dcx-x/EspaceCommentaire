d89a2109e63fbeecac5f4d1e609c31cdbed4fc8a
x-dcx-x <112556623+x-dcx-x@users.noreply.github.com>
2024-01-18 11:32:35 +0100

    push js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,18 @@
 let textprenom = document.getElementById("textprenom");
 let textnom = document.getElementById("textnom");
 let textcom = document.getElementById("textcom");
+let comEnvoyer = document.getElementById("comEnvoyer");
+let button = document.getElementById("envoyer");
+//////////// Commentaire recuperer /////////////
+let nomPrenom = document.getElementById("nomPrenom");                                                                               ;
+let coms = document.getElementById("coms");
+
+let captureComs= textcom.value;
+
+
+
+button.addEventListener("click",function (){
+    textcom.value= coms.innerHTML;
+    console.log(coms.value)
+} )
+
