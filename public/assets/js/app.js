425225448a60b5ce186a6266722491459b561625
x-dcx-x <112556623+x-dcx-x@users.noreply.github.com>
2024-01-18 19:15:50 +0100

    push js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,34 +6,48 @@ let button = document.getElementById("envoyer");
 
 button.addEventListener("click", function () {
 
+    if (textprenom.value.trim() === "" || textnom.value.trim() === "" || textcom.value.trim() === "") {
+        textprenom.value = " ";
+        textnom.value = " ";
+        textcom.value = " ";
+        alert("Veuillez remplir le(s) champ(s) de formulaire(s)");
+    } else {
+        let PrenomComs = document.createElement("p");
+        comEnvoyer.appendChild(PrenomComs);
+        PrenomComs.className = "prenomcom";
+        PrenomComs.innerHTML += textprenom.value
 
+        let nom = document.createElement("span");
+        PrenomComs.appendChild(nom);
+        nom.innerHTML += " " + textnom.value;
 
+       // verification que le champs de com est vide
+        if (textcom.value.trim() !== "") {
+            let coms = document.createElement("p");
+            comEnvoyer.appendChild(coms);
+            coms.className = "com";
+            coms.innerHTML += textcom.value;
+        }
 
-    let PrenomComs = document.createElement("p");
-    comEnvoyer.appendChild(PrenomComs);
-    PrenomComs.className = "prenomcom";
-    PrenomComs.innerHTML += textprenom.value
+        let barre = document.createElement("p");
+        comEnvoyer.appendChild(barre);
+        barre.className = "barre";
+        barre.style.borderBottom = "1px solid black";
 
-    let nom = document.createElement("span")
-    PrenomComs.appendChild(nom);
-    nom.innerHTML += " " + textnom.value;
+        if (textprenom.value.trim() === "" || textnom.value.trim() === "" || textcom.value.trim() === "" || barre.trim("p")) {
+            barre.style.display = "none";
+            barre.style = "none";
+        }
 
-    let coms = document.createElement("p");
-    comEnvoyer.appendChild(coms);
-    coms.className = "com";
-    coms.innerHTML += textcom.value;
-
-
-    let barre = document.createElement("p");
-    comEnvoyer.appendChild(barre);
-    barre.className = "barre";
-    barre.style.borderBottom = "1px solid black";
-
-///////// reset of formulaire//////
-    textprenom.value = " ";
-    textnom.value = " ";
-    textcom.value = " ";
-
-
+        // reset of value formulaire
+        textprenom.value = " ";
+        textnom.value = " ";
+        textcom.value = " ";
+    }
 })
+
+
+
+
+
 
