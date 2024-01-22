abac6f1a8b05dd557118179d21293cbfcebf7bf5
guillaume <112556623+x-dcx-x@users.noreply.github.com>
2024-01-22 11:48:12 +0100

    push js presque final

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,9 @@ let textcom = document.getElementById("textcom");
 let comEnvoyer = document.getElementById("comEnvoyer");
 let button = document.getElementById("envoyer");
 let content = document.getElementById("content");
+let titreCom = document.getElementById("titrecom");
+let ecrireUnCom = document.getElementById("ecrireUnComment")
+
 
 button.addEventListener("click", function () {
 
@@ -11,11 +14,32 @@ button.addEventListener("click", function () {
         textprenom.value = " ";
         textnom.value = " ";
         textcom.value = " ";
-        alert("Veuillez remplir le(s) champ(s) de formulaire(s)");
-    } else {
+
+       show()
+        if(textprenom.value.trim() !== "" || textnom.value.trim() !== "" || textcom.value.trim() !== ""){
+
+            cacherMessageErreur ()
+            // console.log(hidden())
+        }
+
+        }
+
+
+
+        // alert("Veuillez remplir le(s) champ(s) de formulaire(s)");
+
+       // hidden();
+
+
+
+    else {
+        cacherMessageErreur ()
+
+
+
         let PrenomComs = document.createElement("p");
         comEnvoyer.appendChild(PrenomComs);
-        PrenomComs.className = "prenomcom";
+        PrenomComs.className = "prenomCom";
         PrenomComs.innerHTML += textprenom.value
 
         let nom = document.createElement("span");
@@ -36,7 +60,7 @@ button.addEventListener("click", function () {
         comEnvoyer.appendChild(barre);
         barre.className = "barre";
         barre.style.borderBottom = "1px solid black";
-        barre.style.opacity ="10%";
+        barre.style.opacity = "10%";
 
         if (textprenom.value.trim() === "" || textnom.value.trim() === "" || textcom.value.trim() === "") {
             barre.style.display = "none";
@@ -50,3 +74,170 @@ button.addEventListener("click", function () {
     }
 
 })
+
+
+function show (){
+    let message = document.createElement("p");
+    ecrireUnCom.appendChild(message);
+    message.className = "messageError"
+    message.style.display = "flex";
+    message.innerHTML = " Veuillez remplir le(s) champ(s) de formulaire(s) ";
+
+    message.style.display = "flex";
+    message.style.justifyContent = "center";
+    message.style.alignItems = "center";
+    message.style.marginTop = " 0.5vw";
+    message.style.marginLeft = "2vw"
+    message.style.height = "2vw";
+    message.style.backgroundColor = "palevioletred";
+    message.style.borderRadius = "25px";
+    message.style.opacity = "70%";
+
+    // noRepeatError()
+}
+
+
+
+function cacherMessageErreur() {
+    let msgErreurExistant = document.querySelector(".messageError");
+    if (msgErreurExistant) {
+        msgErreurExistant.style.display = "none";
+
+    }
+}
+// function noRepeatError (){
+//     if (show() === 1){
+//         show().style.display ="none"
+//     }
+// }
+
+
+// function show() {
+//     messageErreur.style.display = "flex";
+// }
+//
+// function hidden() {
+//     messageErreur.style.display = "none";
+// }
+
+       ////////////code 02///////////////////
+
+// button.addEventListener("click", function () {
+//     // Réinitialiser tout message d'erreur existant
+//     cacherMessageErreur();
+//
+//     if (textprenom.value.trim() === "" || textnom.value.trim() === "" || textcom.value.trim() === "") {
+//         // alert("Veuillez remplir le(s) champ(s) de formulaire(s)");
+//
+//         // Afficher le message d'erreur
+//         afficherMessageErreur();
+//     } else {
+//         // Votre code existant pour traiter les champs non vides
+//
+//         // Réinitialisation des valeurs du formulaire
+//         textprenom.value = " ";
+//         textnom.value = " ";
+//         textcom.value = " ";
+//     }
+// });
+//
+// function afficherMessageErreur() {
+//     if (!document.querySelector(".messageError")) {
+//         let message = document.createElement("p");
+//         ecrireUnCom.appendChild(message);
+//         message.className = "messageError";
+//         message.innerHTML = " Veuillez remplir le(s) champ(s) de formulaire(s) ";
+//         styliserMessageErreur(message);
+//     }
+// }
+//
+// function cacherMessageErreur() {
+//     let msgErreurExistant = document.querySelector(".messageError");
+//     if (msgErreurExistant) {
+//         msgErreurExistant.style.display = "none";
+//     }
+// }
+//
+// function styliserMessageErreur(message) {
+//     message.style.display = "flex";
+//     message.style.justifyContent = "center";
+//     message.style.alignItems = "center";
+//     message.style.marginTop = "0.5vw";
+//     message.style.marginLeft = "2vw";
+//     message.style.height = "2vw";
+//     message.style.backgroundColor = "palevioletred";
+//     message.style.borderRadius = "25px";
+//     message.style.opacity = "70%";
+// }
+/////////////////fin de code 02 ////////////////
+
+// button.addEventListener("click", function () {
+//     // Réinitialiser tout message d'erreur existant
+//     cacherMessageErreur();
+//
+//     if (textprenom.value.trim() === "" || textnom.value.trim() === "" || textcom.value.trim() === "") {
+//         // Afficher le message d'erreur
+//         afficherMessageErreur();
+//     } else {
+//         // Votre code existant pour traiter les champs non vides
+//
+//         // Créer et afficher le commentaire
+//         let PrenomComs = document.createElement("p");
+//         comEnvoyer.appendChild(PrenomComs);
+//         PrenomComs.className = "prenomCom";
+//         PrenomComs.innerHTML += textprenom.value;
+//
+//         let nom = document.createElement("span");
+//         PrenomComs.appendChild(nom);
+//         nom.innerHTML += " " + textnom.value;
+//
+//         // Vérifier que le champ de commentaire n'est pas vide
+//         if (textcom.value.trim() !== "") {
+//             let coms = document.createElement("p");
+//             comEnvoyer.appendChild(coms);
+//             coms.className = "com";
+//             coms.innerHTML += textcom.value;
+//         }
+//
+//         let barre = document.createElement("p");
+//         comEnvoyer.appendChild(barre);
+//         barre.className = "barre";
+//         barre.style.borderBottom = "1px solid black";
+//         barre.style.opacity = "10%";
+//
+//         // Réinitialisation des valeurs du formulaire
+//         textprenom.value = " ";
+//         textnom.value = " ";
+//         textcom.value = " ";
+//     }
+// });
+//
+// function afficherMessageErreur() {
+//     if (!document.querySelector(".messageError")) {
+//         let message = document.createElement("p");
+//         ecrireUnCom.appendChild(message);
+//         message.className = "messageError";
+//         message.innerHTML = " Veuillez remplir le(s) champ(s) de formulaire(s) ";
+//         styliserMessageErreur(message);
+//     }
+// }
+//
+// function cacherMessageErreur() {
+//     let msgErreurExistant = document.querySelector(".messageError");
+//     if (msgErreurExistant) {
+//         msgErreurExistant.style.display = "none";
+//     }
+// }
+//
+// function styliserMessageErreur(message) {
+//     message.style.display = "flex";
+//     message.style.justifyContent = "center";
+//     message.style.alignItems = "center";
+//     message.style.marginTop = "0.5vw";
+//     message.style.marginLeft = "2vw";
+//     message.style.height = "2vw";
+//     message.style.backgroundColor = "palevioletred";
+//     message.style.borderRadius = "25px";
+//     message.style.opacity = "70%";
+// }
+
